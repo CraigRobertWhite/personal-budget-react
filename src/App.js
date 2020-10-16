bcaa372973ccb494dab791e1785634953967975a
Craig White <craigwhiteofficial@gmail.com>
2020-10-15 21:57:48 -0400

    Integrated Personal Budget files into the React app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Menu from './components/Menu/Menu';
+import Hero from './components/Hero/Hero';
+import Footer from './components/Footer/Footer';
+
+import Home from './views/Home/Home';
+import About from './views/About/About';
+import Login from './views/Login/Login';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Menu/>
+            <Hero/>
+            <div className="mainContainer">
+                <Switch>
+                    <Route path="/about" component={About}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/" component={Home}/>
+                </Switch>
+            </div>
+            <Footer/>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="bottom center" aria-label="Footer">
+            All rights reserved &copy; Craig White
+        </footer>
+    );
+}
+
+export default Footer;
